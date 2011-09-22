cfcfd8963154e3d942f3b4aff87243e466b88add
Sergey Stolyarov <sergei@regolit.com>
2011-09-22 15:14:22 +0600

    fixing issue #14

--- pyrone/static/lang/ru.js
+++ pyrone/static/lang/ru.js
@@ -3,8 +3,16 @@ Ext.ns('Pyrone.tr');
 Pyrone.tr = {
 'AJAX_REQUEST_ERROR': 'Ошибка выполнения AJAX-запроса',
 'BACKUP_RESTORE_COMPLETE': 'Бэкап успешно восстановлен',
+'COMMENT_BODY_IS_REQUIRED': 'Текст комментария обязателен',
 'COMMENT_DISPLAY_NAME_IS_REQUIRED': 'Имя автора комментария обязательно к заполнению',
 'COMMENT_IS_WAITING_FOR_APPROVAL': 'Комментарий ожидает одобрения администратора сайта',
 'FIELD_IS_REQUIRED': 'Поле обязательно для заполнения',
-'FILE_NAME_ALREADY_EXISTS_IN_THE_STORAGE': 'Файл с таким именем уже есть в хранилище'
+'FIELD_MUST_BE_ALPHA_NUM': 'Поле должно содержать только буквенно-цифровые символы',
+'FILE_NAME_ALREADY_EXISTS_IN_THE_STORAGE': 'Файл с таким именем уже есть в хранилище',
+'PASSWORDS_DONT_MATCH': 'Пароли не совпадают',
+'SELECT_FILE_TO_UPLOAD': 'Выберите файл для загрузки',
+'SELECT_ITEMS_FIRST': 'Сначала выберите элементы',
+'SETTINGS_SAVED': 'Настройки успешно сохранены',
+'TWITTER_AUTH_IS_NOT_WORKING': 'Аутентификация через Twitter не работает',
+'YOUR_PROFILE_HAS_BEEN_UPDATED': 'Ваш профиль был успешно обновлён'
 };
