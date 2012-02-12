c40417955162f800ce621756a097b70f7f195c14
Sergey Stolyarov <sergei@regolit.com>
2012-02-12 18:31:24 +0700

    version increased

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ except ImportError:
     pass
 
 setup(name='pyrone',
-      version='0.2.8',
+      version='0.2.9',
       license='New BSD License',
       description='pyrone',
       long_description=README + '\n\n' +  CHANGES,
